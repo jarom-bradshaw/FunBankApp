944654fcb9501f119a247e8f651415377ef4e461
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-25 17:39:59 -0600

    Initial project setup with Spring Boot and JDBC config
    Tested against a local instance of a mySQL server, changed git ignore to save credentials safely.

---
+++ src/main/java/com/jarom/funbankapp/FunBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.jarom.funbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarom/funbankapp/FunBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarom.funbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
