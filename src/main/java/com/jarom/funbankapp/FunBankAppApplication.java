c920ea513103e1bbe44fce956e029e6970e75c48
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-03-26 16:13:54 -0600

    Add eable config properties, maybe take back if needed.

--- src/main/java/com/jarom/funbankapp/FunBankAppApplication.java
+++ src/main/java/com/jarom/funbankapp/FunBankAppApplication.java
@@ -1,13 +1,15 @@
 package com.jarom.funbankapp;
 
+import com.jarom.funbankapp.config.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class FunBankAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FunBankAppApplication.class, args);
 	}
-
 }
+
